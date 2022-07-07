b595ac1af739710ac140dec01ff288c9a0583bd1
Joshua Gerrish <jgerrish@gmail.com>
2022-07-07 06:47:38 +0000

    Check that there are no path separators in GitHub API results. (#3)
    Check that there are no path separators in GitHub API results.
    Add tests for the checks.

--- internal/helper.go
+++ internal/helper.go
@@ -1,12 +1,40 @@
 package internal
 
 import (
+	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"os/exec"
+	"strings"
 	"time"
 )
+
+// Check if a string is safe for substituting in a path.  If the
+// string has substrings like ../ in it, it's not safe.  Check for
+// OS-specific separator and the forward slash.  The code uses forward
+// slashes before final substitution with filepath.FromSlash.  If
+// we're getting unsafe strings, things have gone wrong, just panic.
+// If the subpath had no separators, return the string.
+func sanitizedFilePath(subpath string) string {
+	if strings.Contains(subpath, fmt.Sprintf("%c", os.PathSeparator)) {
+		panic(fmt.Sprintf("Unsafe result returned from github API: %s", subpath))
+	}
+
+	// Also check for a forward slash
+	if strings.Contains(subpath, "/") {
+		panic(fmt.Sprintf("Unsafe result returned from github API: %s", subpath))
+	}
+
+	return subpath
+}
+
+// Replace all separator characters in a string
+// This replaces both OS-specific file path separators and foward slashes.
+func replaceSeparatorsFilePath(path string, osPathSeparator rune, replacement string) string {
+	path = strings.ReplaceAll(path, fmt.Sprintf("%c", osPathSeparator), replacement)
+	return strings.ReplaceAll(path, "/", replacement)
+}
 
 func runCmd(cmd string, args []string) error {
 	pwd, _ := os.Getwd()
--- internal/save.go
+++ internal/save.go
@@ -5,41 +5,40 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/google/go-github/v42/github"
 )
 
 func (r *Backup) repoJsonPath(repo *github.Repository) string {
-	return fmt.Sprintf("%s/%s/repo/%s/%s.json", r.BackupDir, r.self.GetLogin(), repo.GetName(), repo.GetName())
+	return fmt.Sprintf("%s/%s/repo/%s/%s.json", r.BackupDir, sanitizedFilePath(r.self.GetLogin()), sanitizedFilePath(repo.GetName()), sanitizedFilePath(repo.GetName()))
 }
 
 func (r *Backup) repoGitZipPath(repo *github.Repository) string {
-	return fmt.Sprintf("%s/%s/repo/%s/%s.git.zip", r.BackupDir, r.self.GetLogin(), repo.GetName(), repo.GetName())
+	return fmt.Sprintf("%s/%s/repo/%s/%s.git.zip", r.BackupDir, sanitizedFilePath(r.self.GetLogin()), sanitizedFilePath(repo.GetName()), sanitizedFilePath(repo.GetName()))
 }
 
 func (r *Backup) repoIssueJsonPath(repo *github.Repository, issue *github.Issue) string {
-	return fmt.Sprintf("%s/%s/repo/%s/issue/%d/%d.json", r.BackupDir, r.self.GetLogin(), repo.GetName(), issue.GetID(), issue.GetID())
+	return fmt.Sprintf("%s/%s/repo/%s/issue/%d/%d.json", r.BackupDir, sanitizedFilePath(r.self.GetLogin()), sanitizedFilePath(repo.GetName()), issue.GetID(), issue.GetID())
 }
 
 func (r *Backup) repoIssueCommentJsonPath(repo *github.Repository, issue *github.Issue, comment *github.IssueComment) string {
-	return fmt.Sprintf("%s/%s/repo/%s/issue/%d/comment/%d.json", r.BackupDir, r.self.GetLogin(), repo.GetName(), issue.GetID(), comment.GetID())
+	return fmt.Sprintf("%s/%s/repo/%s/issue/%d/comment/%d.json", r.BackupDir, sanitizedFilePath(r.self.GetLogin()), sanitizedFilePath(repo.GetName()), issue.GetID(), comment.GetID())
 }
 
 func (r *Backup) starJsonPath(star *github.StarredRepository) string {
-	return fmt.Sprintf("%s/%s/star/%s.json", r.BackupDir, r.self.GetLogin(), strings.ReplaceAll(star.GetRepository().GetFullName(), "/", "_"))
+	return fmt.Sprintf("%s/%s/star/%s.json", r.BackupDir, sanitizedFilePath(r.self.GetLogin()), replaceSeparatorsFilePath(star.GetRepository().GetFullName(), os.PathSeparator, "_"))
 }
 
 func (r *Backup) followerJsonPath(user *github.User) string {
-	return fmt.Sprintf("%s/%s/follower/%s.json", r.BackupDir, r.self.GetLogin(), user.GetLogin())
+	return fmt.Sprintf("%s/%s/follower/%s.json", r.BackupDir, sanitizedFilePath(r.self.GetLogin()), sanitizedFilePath(user.GetLogin()))
 }
 
 func (r *Backup) followingJsonPath(user *github.User) string {
-	return fmt.Sprintf("%s/%s/following/%s.json", r.BackupDir, r.self.GetLogin(), user.GetLogin())
+	return fmt.Sprintf("%s/%s/following/%s.json", r.BackupDir, sanitizedFilePath(r.self.GetLogin()), sanitizedFilePath(user.GetLogin()))
 }
 
 func (r *Backup) gistJsonPath(data *github.Gist) string {
-	return fmt.Sprintf("%s/%s/gist/%s.json", r.BackupDir, r.self.GetLogin(), data.GetID())
+	return fmt.Sprintf("%s/%s/gist/%s.json", r.BackupDir, sanitizedFilePath(r.self.GetLogin()), sanitizedFilePath(data.GetID()))
 }
 
 func getPathBaseName(path string) string {
@@ -49,7 +48,7 @@ func getPathBaseName(path string) string {
 }
 
 func (r *Backup) repoZipPath(repo *github.Repository) string {
-	return fmt.Sprintf("%s/%s/repo/%s/repo.zip", r.BackupDir, r.self.GetLogin(), repo.GetName())
+	return fmt.Sprintf("%s/%s/repo/%s/repo.zip", r.BackupDir, sanitizedFilePath(r.self.GetLogin()), sanitizedFilePath(repo.GetName()))
 }
 
 func (r *Backup) SaveRepoZip(repo *github.Repository) {
